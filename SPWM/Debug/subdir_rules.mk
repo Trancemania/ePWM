4007c3a3f2f5502e86e3084d50feb81410750384
lvhuixiang <75711678+heri9805@users.noreply.github.com>
2021-03-26 11:22:07 +0800

    MOEA/D

---
+++ SPWM/Debug/subdir_rules.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+%.obj: ../%.asm $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="D:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include" --include_path="D:/Gitub/ePWM/inc/headers" --include_path="D:/Gitub/ePWM/inc/common" --include_path="D:/Gitub/ePWM/inc/IQmath" --include_path="D:/Gitub/ePWM/inc/FPUfastRTS" --define=_DEBUG --define=LARGE_MODEL -g --diag_suppress=10063 --diag_warning=225 --issue_remarks --verbose_diagnostics --quiet --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+%.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="D:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include" --include_path="D:/Gitub/ePWM/inc/headers" --include_path="D:/Gitub/ePWM/inc/common" --include_path="D:/Gitub/ePWM/inc/IQmath" --include_path="D:/Gitub/ePWM/inc/FPUfastRTS" --define=_DEBUG --define=LARGE_MODEL -g --diag_suppress=10063 --diag_warning=225 --issue_remarks --verbose_diagnostics --quiet --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
---
+++ three-phase spwm/Debug/DSP2833x_GlobalVariableDefs.d
@@ -0,0 +1,43 @@
+# FIXED
+
+DSP2833x_GlobalVariableDefs.obj: ../DSP2833x_GlobalVariableDefs.c
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_Device.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_Adc.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_DevEmu.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_CpuTimers.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_ECan.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_ECap.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_DMA.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_EPwm.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_EQep.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_Gpio.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_I2c.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_Mcbsp.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_PieCtrl.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_PieVect.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_Spi.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_Sci.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_SysCtrl.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_XIntrupt.h
+DSP2833x_GlobalVariableDefs.obj: D:/Gitub/ePWM/inc/headers/DSP2833x_Xintf.h
+
+../DSP2833x_GlobalVariableDefs.c: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_Device.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_Adc.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_DevEmu.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_CpuTimers.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_ECan.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_ECap.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_DMA.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_EPwm.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_EQep.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_Gpio.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_I2c.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_Mcbsp.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_PieCtrl.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_PieVect.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_Spi.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_Sci.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_SysCtrl.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_XIntrupt.h: 
+D:/Gitub/ePWM/inc/headers/DSP2833x_Xintf.h: 
